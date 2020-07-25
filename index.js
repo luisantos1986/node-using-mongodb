fd42c18052d2ceeae25818a50059963a54b27375
Luis Santos <luis.eugenio.santos@accenture.com>
2020-07-25 18:55:58 -0500

    Adding mongoose to index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express();
 const PORT = 4000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
