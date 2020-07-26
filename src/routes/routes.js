e121292e34d7924e4050bdb11a24a6f6306a1be7
Luis Santos <luis.eugenio.santos@accenture.com>
2020-07-26 16:29:33 -0500

    Finishing module 5

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,27 @@
 const { addnewProduct, 
         getProductWithID,
-        getProducts 
+        getProducts,
+        updateProduct,
+        deleteProduct
     } = require('../controllers/controllers.js')
 
 const routes = (app) => {
     app.route('/products')
         // get multiple products
         .get(getProducts)
-        
+
         // Post endpoint
         .post(addnewProduct);
     app.route('/products/:ProductID')
         // get a specifict product with ID
-        .get(getProductWithID);
+        .get(getProductWithID)
+
+        // updating a specific product
+        .put(updateProduct)
+        
+        // deleting a specific product
+        .delete(deleteProduct);
+        
     
     
 }
