1220a506dddd6f871035c6dd6dacaade826741f2
Luis Santos <luis.eugenio.santos@accenture.com>
2020-07-26 16:19:37 -0500

    Finishing module 4

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,20 @@
-const { addnewProduct } = require('../controllers/controllers.js')
+const { addnewProduct, 
+        getProductWithID,
+        getProducts 
+    } = require('../controllers/controllers.js')
 
 const routes = (app) => {
     app.route('/products')
-
+        // get multiple products
+        .get(getProducts)
+        
         // Post endpoint
         .post(addnewProduct);
+    app.route('/products/:ProductID')
+        // get a specifict product with ID
+        .get(getProductWithID);
+    
+    
 }
 
 module.exports = routes;
